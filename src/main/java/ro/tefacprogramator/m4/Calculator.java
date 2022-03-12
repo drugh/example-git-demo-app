a91c000bcedb425e64da9f8ad418b9a0bae808dc
Mihai <vlada.mihai.catalin@gmail.com>
2022-03-12 15:46:32 +0200

    homepage

--- src/main/java/ro/tefacprogramator/m4/Calculator.java
+++ src/main/java/ro/tefacprogramator/m4/Calculator.java
@@ -4,13 +4,16 @@ public class Calculator {
     public static int add(int a, int b) {
         return a + b;
     }
+    public static double add(double a, double b) {
+        return a + b;
+    }
     public static int subtract(int a, int b) {
-        return a - b;
+        return a + b;
     }
     public static int multiply(int a, int b) {
-        return a * b;
+        return a + b;
     }
     public static int divide(int a, int b) {
-        return a / b;
+        return a + b;
     }
 }
---
+++ src/main/java/ro/tefacprogramator/m4/Homepage.java
@@ -0,0 +1,7 @@
+package ro.tefacprogramator.m4;
+
+public class Homepage {
+    public void showPageContent() {
+        System.out.println("This is the homepage");
+    }
+}
